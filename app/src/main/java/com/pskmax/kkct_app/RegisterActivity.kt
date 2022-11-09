c88629612a235fa5a8271f4f09bcf16796ef18bc
Napat Jirarattanakunchai <69129078+Napatji@users.noreply.github.com>
2022-11-09 11:53:46 +0700

    ปรับแก้ Register ให้มี theme เดียวกับ Login อันใหม่ คอมเมนต์ของโอ๊ตไว้แปปนึงเพื่อให้เรารันแอปผ่าน

--- app/src/main/java/com/pskmax/kkct_app/RegisterActivity.kt
+++ app/src/main/java/com/pskmax/kkct_app/RegisterActivity.kt
@@ -6,11 +6,11 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
+//import com.android.volley.Request
+//import com.android.volley.Response
+//import com.android.volley.toolbox.JsonObjectRequest
+//import com.android.volley.toolbox.StringRequest
+//import com.android.volley.toolbox.Volley
 import com.pskmax.kkct_app.data.Register
 import org.json.JSONObject
 
@@ -53,31 +53,32 @@ class RegisterActivity : AppCompatActivity() {
 
         val regScreen = Register()
         btnRegister!!.setOnClickListener{
-            register00(editEmail.toString(),editEmail.toString(), editPassword.toString())
-//            if (!isEmailValid((editEmail?.text).toString())){
-//                println("Your Email is not correct")
-//            }
-//            else if (editPassword?.length()!! < 8){
-//                println("Your pass must be between 8-15 characters")
-//            }
-//            else if (!checkPassword((editPassword?.text).toString())){
-//                println("Your password must have at least 1 Uppercase, Lowercase and Numeric")
-//            }
-//            else if ((editPassword?.text).toString() != (editCPassword?.text).toString()){
-//                println("Your confirm password is not correct")
-//            }
-//            else if (editId?.length()!! < 13){
-//                println("Your Citizen ID must have 13 characters")
-//            }
-//            else{
-//                val intent = Intent(this@RegisterActivity,LoginActivity::class.java)
-//                regScreen.updateUserInfo((editEmail?.text).toString(),(editPassword?.text).toString(),(editId?.text).toString())
-//                // ลบ Stack ของ Intent///////////////
-//                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
-//                startActivity(intent)
-//                this.finish();
-//                /////////////////////////////////////
-//            }
+            //ของโอ๊ต
+//            register00(editEmail.toString(),editEmail.toString(), editPassword.toString())
+            if (!isEmailValid((editEmail?.text).toString())){
+                println("Your Email is not correct")
+            }
+            else if (editPassword?.length()!! < 8){
+                println("Your pass must be between 8-15 characters")
+            }
+            else if (!checkPassword((editPassword?.text).toString())){
+                println("Your password must have at least 1 Uppercase, Lowercase and Numeric")
+            }
+            else if ((editPassword?.text).toString() != (editCPassword?.text).toString()){
+                println("Your confirm password is not correct")
+            }
+            else if (editId?.length()!! < 13){
+                println("Your Citizen ID must have 13 characters")
+            }
+            else{
+                val intent = Intent(this@RegisterActivity,LoginActivity::class.java)
+                regScreen.updateUserInfo((editEmail?.text).toString(),(editPassword?.text).toString(),(editId?.text).toString())
+                // ลบ Stack ของ Intent///////////////
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                this.finish();
+                /////////////////////////////////////
+            }
         }
 
         btnToLogin!!.setOnClickListener{
@@ -89,33 +90,33 @@ class RegisterActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
-
-    private fun register00(name: String,username : String ,password : String){
-
-
-        val regJson = JSONObject()
-        regJson.put("name","Thanapat01")
-        regJson.put("username","oat01")
-        regJson.put("password", "password")
-
-        val url = "http://10.0.2.2:8080/api/signup"
-        val jsonRequest = object : JsonObjectRequest(
-            Request.Method.POST, url, regJson,
-            Response.Listener{
-                response -> Log.d("Respond",response.toString())
-            },
-            Response.ErrorListener{ error ->
-                Log.d("Response",error.toString())
-                return@ErrorListener
-            }
-        ){
-            override fun getBodyContentType(): String {
-                return "application/json"
-            }
-          }
-
-        val queue = Volley.newRequestQueue(this)
-        queue.add(jsonRequest)
-    }
+    //ของโอ๊ต
+//    private fun register00(name: String,username : String ,password : String){
+//
+//
+//        val regJson = JSONObject()
+//        regJson.put("name","Thanapat01")
+//        regJson.put("username","oat01")
+//        regJson.put("password", "password")
+//
+//        val url = "http://10.0.2.2:8080/api/signup"
+//        val jsonRequest = object : JsonObjectRequest(
+//            Request.Method.POST, url, regJson,
+//            Response.Listener{
+//                response -> Log.d("Respond",response.toString())
+//            },
+//            Response.ErrorListener{ error ->
+//                Log.d("Response",error.toString())
+//                return@ErrorListener
+//            }
+//        ){
+//            override fun getBodyContentType(): String {
+//                return "application/json"
+//            }
+//          }
+//
+//        val queue = Volley.newRequestQueue(this)
+//        queue.add(jsonRequest)
+//    }
 
 }
