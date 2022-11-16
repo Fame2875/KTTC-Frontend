116309a323d9a5ce44dd618b0907ebbb1a6521f4
oathathat <69292322+oathathat@users.noreply.github.com>
2022-11-16 17:13:09 +0700

    volley

--- app/src/main/java/com/pskmax/kkct_app/RegisterActivity.kt
+++ app/src/main/java/com/pskmax/kkct_app/RegisterActivity.kt
@@ -6,6 +6,10 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 //import com.android.volley.Request
 //import com.android.volley.Response
 //import com.android.volley.toolbox.JsonObjectRequest
@@ -53,8 +57,7 @@ class RegisterActivity : AppCompatActivity() {
 
         val regScreen = Register()
         btnRegister!!.setOnClickListener{
-            //ของโอ๊ต
-//            register00(editEmail.toString(),editEmail.toString(), editPassword.toString())
+
             if (!isEmailValid((editEmail?.text).toString())){
                 println("Your Email is not correct")
             }
@@ -71,6 +74,9 @@ class RegisterActivity : AppCompatActivity() {
                 println("Your Citizen ID must have 13 characters")
             }
             else{
+                //ของโอ๊ต
+                signUp((editEmail?.text).toString(),(editPassword?.text).toString(),(editId?.text).toString())
+
                 val intent = Intent(this@RegisterActivity,LoginActivity::class.java)
                 regScreen.updateUserInfo((editEmail?.text).toString(),(editPassword?.text).toString(),(editId?.text).toString())
                 // ลบ Stack ของ Intent///////////////
@@ -91,32 +97,35 @@ class RegisterActivity : AppCompatActivity() {
         }
     }
     //ของโอ๊ต
-//    private fun register00(name: String,username : String ,password : String){
-//
-//
-//        val regJson = JSONObject()
-//        regJson.put("name","Thanapat01")
-//        regJson.put("username","oat01")
-//        regJson.put("password", "password")
-//
-//        val url = "http://10.0.2.2:8080/api/signup"
-//        val jsonRequest = object : JsonObjectRequest(
-//            Request.Method.POST, url, regJson,
-//            Response.Listener{
-//                response -> Log.d("Respond",response.toString())
-//            },
-//            Response.ErrorListener{ error ->
-//                Log.d("Response",error.toString())
-//                return@ErrorListener
-//            }
-//        ){
-//            override fun getBodyContentType(): String {
-//                return "application/json"
-//            }
-//          }
-//
-//        val queue = Volley.newRequestQueue(this)
-//        queue.add(jsonRequest)
-//    }
+    private fun signUp(email: String  ,password : String , Cid: String)  {
+
+        val regJson = JSONObject()
+        // รอค่า key ที่ database ที่ถูกต้องอีกที
+        regJson.put("email",email)
+        regJson.put("password",password)
+        regJson.put("citizenID", Cid)
+
+        // 10.0.2.2 คือค่า loopback ของ android studio , 8080 คือ port
+        val url = "http://10.0.2.2:8093/api/signup"
+        val jsonRequest = object : JsonObjectRequest(
+            Request.Method.POST, url, regJson,
+            Response.Listener{
+                    response -> Log.d("Respond",response.toString())
+            },
+            Response.ErrorListener{ error ->
+                Log.d("Response",error.toString())
+                return@ErrorListener
+            }
+        ){
+            override fun getBodyContentType(): String {
+                return "application/json"
+            }
+
+        }
+
+        val queue = Volley.newRequestQueue(this)
+        queue.add(jsonRequest)
+
+    }
 
 }
