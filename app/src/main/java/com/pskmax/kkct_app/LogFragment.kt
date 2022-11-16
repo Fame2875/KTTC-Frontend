ec0a74ce60cda44a9c9a0ce430c9a540514511a1
Napat Jirarattanakunchai <69129078+Napatji@users.noreply.github.com>
2022-11-16 22:29:39 +0700

    updated

--- app/src/main/java/com/pskmax/kkct_app/LogFragment.kt
+++ app/src/main/java/com/pskmax/kkct_app/LogFragment.kt
@@ -8,16 +8,6 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LogFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LogFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -50,14 +40,8 @@ class LogFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
 
-        logRecyclerView = findViewById<RecyclerView>(R.id.logRecyclerView)
-        logRecyclerView!!.layoutManager = LinearLayoutManager(this)
-        val logAdapter = LogRecycleView(log,this)
-        logRecyclerView!!.adapter = logAdapter
+        }
     }
 
     override fun onCreateView(
@@ -68,23 +52,11 @@ class LogFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_log, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LogFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LogFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        logRecyclerView = view.findViewById<RecyclerView>(R.id.logRecyclerView)
+        logRecyclerView!!.layoutManager = LinearLayoutManager(requireContext())
+        val logAdapter = LogRecycleView(log,requireContext())
+        logRecyclerView!!.adapter = logAdapter
     }
 }
