2afb0ce05e1454a68b2d2c172ea6ca81e6f5e3a5
Napat Jirarattanakunchai <69129078+Napatji@users.noreply.github.com>
2022-11-17 20:10:26 +0700

    เพิ่มหน้า Transaction กับ Unpaid เหลือลองเชื่อม

--- app/src/main/java/com/pskmax/kkct_app/HomeFragment.kt
+++ app/src/main/java/com/pskmax/kkct_app/HomeFragment.kt
@@ -6,16 +6,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 
 class HomeFragment : Fragment() {
 
     private var profileButton : ImageView? = null
+    private var transactionButton : Button? = null
+    private var unpaidButton : Button? = null
 
-    private fun toProfile(){
+    private fun changeFragments(fragment : Fragment){
         val fragmentManager = activity?.supportFragmentManager
         val fragmentTransaction = fragmentManager?.beginTransaction()
-        fragmentTransaction?.replace(R.id.frame_layout,ProfileFragment())
+        fragmentTransaction?.replace(R.id.frame_layout,fragment)
         fragmentTransaction?.commit()
     }
 
@@ -34,11 +37,21 @@ class HomeFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
         super.onViewCreated(view, savedInstanceState)
         profileButton = view.findViewById(R.id.profileUser)
+        transactionButton = view.findViewById(R.id.seeTrans)
+        unpaidButton = view.findViewById(R.id.seeUnpaid)
+
         profileButton!!.setOnClickListener {
-            toProfile()
+            changeFragments(ProfileFragment())
+        }
+
+        transactionButton!!.setOnClickListener {
+            changeFragments(TransactionsFragment())
+        }
+
+        unpaidButton!!.setOnClickListener {
+            changeFragments(UnpaidFragment())
         }
     }
 }
