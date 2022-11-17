e3adf3db64657b59558480e0eeb5445ad855ca5c
HaNiBaHei <chinnapat2556@hotmail.com>
2022-11-17 20:16:41 +0700

    update setting screen nid noi cuz I mai ru wa ja add ar rai

--- app/src/main/java/com/pskmax/kkct_app/SettingsFragment.kt
+++ app/src/main/java/com/pskmax/kkct_app/SettingsFragment.kt
@@ -2,10 +2,13 @@ package com.pskmax.kkct_app
 
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+
     }
 }
